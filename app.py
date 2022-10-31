8f197751b8bd43d6c200d74c3234352fb6ec41e9
satyazmx <thakursatya6@gmail.com>
2022-10-31 17:14:28 +0530

    update port number

--- app.py
+++ app.py
@@ -69,5 +69,5 @@ def webcam():
     except Exception as e:
         raise FaceDetectionException(e,sys)
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
